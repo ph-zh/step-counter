330acf8c2a5834f129f31bbf9220583c31c47045
ph.zh <p.zhavoronkov@gmail.com>
2024-05-25 19:20:26 +0200

    readme updated with list of tasks requested

---
+++ src/StepTracker.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class StepTracker {
+    Scanner scanner;
+
+    MonthData[] monthToData = new MonthData[12];
+
+    StepTracker(Scanner scan) {
+        scanner = scan;
+    }
+}
